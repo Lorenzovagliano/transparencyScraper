bfefbf0100c3db4182f49caf9faa3a6f3ca42f3e
Lorenzo Vagliano <lorenzo.ventura.vagliano@cern.ch>
2025-05-26 22:25:35 +0200

    feat: Use docker
    Signed-off-by: vaglianolorenzo@proton.me

--- desafio_01/gov_scraper/settings.py
+++ desafio_01/gov_scraper/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -128,8 +129,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # gov_scraper/settings.py
 
 # CELERY SETTINGS
-CELERY_BROKER_URL = 'redis://localhost:6379/0'  # Default Redis URL
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0' # Alternative: Store results in Redis
+CELERY_BROKER_URL = os.environ.get('REDIS_URL', 'redis://localhost:6379/0')
+CELERY_RESULT_BACKEND = os.environ.get('REDIS_URL', 'redis://localhost:6379/0')
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
